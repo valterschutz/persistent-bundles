cb6bffce08453f0f124bd3eabd28208863db65d1
Valter Schütz <valterschutz@proton.me>
2026-06-25 15:22:05 +0200

    ✨ save/load metadata

--- src/persistent_bundles/api.py
+++ src/persistent_bundles/api.py
@@ -1,26 +1,47 @@
 from collections.abc import Mapping
+import json
 from pathlib import Path
+from typing import Any
 
 from persistent_bundles.types import Loadable, Savable
 
 
-def save_bundle(obj: Savable, path: Path) -> None:
+def save_bundle(
+    obj: Savable,
+    path: Path,
+    metadata: Mapping[str, Any] | None = None,  # pyright: ignore[reportExplicitAny]
+) -> None:
     """Save an object and the metadata needed to load it later."""
     assert str(path).endswith(".bundle")
+
     object_path = path / "object"
     object_path.mkdir(parents=True)
     obj.save(object_path)
+
     with (path / "class_name.txt").open("w") as f:
         f.write(obj.__class__.__name__)
+
+    if metadata is not None:
+        with (path / "metadata.json").open("w") as f:
+            json.dump(metadata, f)
 
 
 def load_bundle(
     path: Path,
     class_mapping: Mapping[str, type[Loadable]],
-) -> Loadable:
+) -> tuple[Loadable, Mapping[str, Any]]:  # pyright: ignore[reportExplicitAny]
     """Load a bundle object, returning both the object and metadata."""
     assert str(path).endswith(".bundle")
+
     with (path / "class_name.txt").open("r") as f:
         class_name = f.read()
+
     obj = class_mapping[class_name].load(path / "object")
-    return obj
+
+    if (path / "metadata.json").exists():
+        with (path / "metadata.json").open("r") as f:
+            metadata = json.load(f)
+    else:
+        metadata = {}
+
+    return obj, metadata
--- tests/test_all.py
+++ tests/test_all.py
@@ -32,7 +32,16 @@ def test_can_save_and_load_my_object(tmp_path: Path):
     obj = MyObject(number=42)
     save_bundle(obj, tmp_path / "obj.bundle")
     del obj
-    obj = load_bundle(tmp_path / "obj.bundle", REGISTERED_CLASSES)
+    obj, _metadata = load_bundle(tmp_path / "obj.bundle", REGISTERED_CLASSES)
 
     assert isinstance(obj, MyObject)
     assert obj.number == 42
+
+
+def test_can_save_and_load_metadata(tmp_path: Path):
+    obj = MyObject(number=42)
+    save_bundle(obj, tmp_path / "obj.bundle", metadata={"some data": 43})
+    del obj
+    _obj, metadata = load_bundle(tmp_path / "obj.bundle", REGISTERED_CLASSES)
+
+    assert metadata == {"some data": 43}
